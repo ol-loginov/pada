b550ef74b993ab0e2e7f948781a2d3373f776d48
Oleg Loginov <ol.loginov@gmail.com>
2013-06-10 20:12:16 +0400

    grammar changes

--- pada/pada-compiler/src/test/java/TestRun.java
+++ pada/pada-compiler/src/test/java/TestRun.java
@@ -1,13 +1,12 @@
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.BufferedTokenStream;
 import org.junit.Test;
+import pada.compiler.UnitCodeGenerator;
 import pada.compiler.UnitErrorListener;
-import pada.compiler.UnitTreeWriter;
 import pada.compiler.antlr4.PadaLexer;
 import pada.compiler.antlr4.PadaParser;
 
 import java.io.IOException;
-import java.io.StringWriter;
 import java.net.URISyntaxException;
 
 import static org.junit.Assert.assertEquals;
@@ -24,7 +23,7 @@ public class TestRun {
 
     @Test
     public void testAsset() throws IOException, URISyntaxException {
-        testAsset("0005");
+        testAsset("0006");
     }
 
     // /home/oregu/Projects/pada/github/pada/pada-compiler/src/test/java/TestRun.java:[38,36] ')' expected
@@ -37,8 +36,10 @@ public class TestRun {
         parser.addErrorListener(parserError);
         PadaParser.UnitContext unit = parser.unit();
 
+        UnitCodeGenerator sourceGenerator = new UnitCodeGenerator();
+
         String expected = TestHelper.readAsset(assetName + ".tree");
-        String actual = unit.accept(new UnitTreeWriter<>(new StringWriter())).toString();
+        String actual = "";
         assertEquals("validate tree of " + assetName, expected, actual + TestHelper.join(parserError.getErrorList(), "\n"));
     }
 }
